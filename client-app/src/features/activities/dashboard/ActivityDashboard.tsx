598129a37a685accfc2d734d206e71f0f8746417
apoca <apocalysm1@gmail.com>
2024-05-29 16:24:04 +0200

    Added more functionality such as delete and add/update

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivitList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+interface Props{
+    activities: Activity[];
+    selectedActivity : Activity | undefined;
+    selectActivty : (id: string) =>void;
+    cancelSelectActivity : () => void;
+    editMode: boolean;
+    openForm: (id: string) => void ;
+    closeForm: () => void 
+    createOrEdit: (activity: Activity) => void
+    deleteActivity: (id: string) => void
+}
+
+export default function ActivityDashboard({activities, selectedActivity, 
+    selectActivty, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props){
+    return(
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivitList activities={activities} selectActivty={selectActivty} deleteActivity={deleteActivity}/>
+            </Grid.Column>
+            <GridColumn width='6'>
+                {selectedActivity && !editMode &&
+                <ActivityDetails activity={selectedActivity} 
+                cancelSelectActivity={cancelSelectActivity}
+                openForm={openForm}/>}
+                {editMode &&
+                <ActivityForm closeForm={closeForm} activity ={selectedActivity} createOrEdit={createOrEdit} />}
+            </GridColumn>
+        </Grid>
+    )
+}
