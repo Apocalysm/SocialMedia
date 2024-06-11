8599f86b46259148a221d689e97932e564433586
apoca <apocalysm1@gmail.com>
2024-06-11 13:02:53 +0200

    Added a agent

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,14 +15,19 @@ interface Props{
     closeForm: () => void 
     createOrEdit: (activity: Activity) => void
     deleteActivity: (id: string) => void
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, 
-    selectActivty, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props){
+    selectActivty, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}: Props){
     return(
         <Grid>
             <Grid.Column width='10'>
-                <ActivitList activities={activities} selectActivty={selectActivty} deleteActivity={deleteActivity}/>
+                <ActivitList 
+                    activities={activities} 
+                    selectActivty={selectActivty} 
+                    deleteActivity={deleteActivity}
+                    submitting = {submitting}/>
             </Grid.Column>
             <GridColumn width='6'>
                 {selectedActivity && !editMode &&
@@ -30,7 +35,11 @@ export default function ActivityDashboard({activities, selectedActivity,
                 cancelSelectActivity={cancelSelectActivity}
                 openForm={openForm}/>}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity ={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity ={selectedActivity} 
+                    createOrEdit={createOrEdit} 
+                    submitting = {submitting} />}
             </GridColumn>
         </Grid>
     )
