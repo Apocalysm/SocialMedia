19e054d33585afa9dc8793b69f4c1c41ff4ada85
apoca <apocalysm1@gmail.com>
2024-06-15 13:13:27 +0200

    Added route

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,22 +1,20 @@
-
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
+import { NavLink } from "react-router-dom";
 
 
 
 export default observer(function NavBar() {
-    const {activityStore} = useStore();
     return(
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' header>
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                     MenuPage
                 </Menu.Item>
-                <Menu.Item name="Activites" />
+                <Menu.Item as={NavLink} to='/activities' name="Activites" />
                 <Menu.Item>
-                    <Button onClick={() => activityStore.openForm()}positive content='Create Activit' />
+                    <Button as={NavLink} to='/createActivity'  positive content='Create Activit' />
                 </Menu.Item>
             </Container>
         </Menu>
